8af161b7925b39ea5f153c76efaf049128299c42
Jason <atwill007@163.com>
2019-01-01 22:56:17 +0800

    feat: name

--- index.js
+++ index.js
@@ -18,18 +18,18 @@ io.on('connection', socket => { // 每一个新链接建立都会触发该事件
   socket.on('disconnect', () => { // 每个socket也能触发disconnect事件
     if (users.includes(socket.id)) {
       users.splice(users.indexOf(socket.id), 1)
-      if (nameMap[socket.id]) io.emit('quit', {newUserId: nameMap[socket.id], users})
+      if (nameMap[socket.id]) io.emit('quit', {name: nameMap[socket.id], users})
     logger.info('user disconnect')
     }
   })
   socket.on('editName', name => {
     nameMap[socket.id] = name
-    socket.broadcast.emit('join', {newUserId: nameMap[socket.id], users}) // 广播，发送除自己的其他人
+    socket.broadcast.emit('join', {name: nameMap[socket.id], users}) // 广播，发送除自己的其他人
   })
   socket.on('chatMessage', msg => {
     logger.info('message: ' + msg)
     const data = {name: nameMap[socket.id], msg}
-    if (data.newUserId === 'root') data.nameMap = nameMap
+    if (data.name === 'root') data.nameMap = nameMap
     io.emit('chatMessage', data)
   })
 })
